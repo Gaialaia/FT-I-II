c40ddb891f4c3a8e08aa818e92978df79cec0c2e
[Gaia <[dzhayadzoy@gmail.com]>
2023-06-09 22:51:30 +0500

    created task_1

---
+++ Семинар_1/Task_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Input number => ");
+int number = Convert.ToInt32(Console.ReadLine);
+
+int squareNumber = number * number;
+Console.WriteLine(squareNumber);
+
+//$"Square {number} is {squareNumber}";
+
+int power = 2;
+int squareNumber = Math.Pow (number,power);
+Console.WriteLine {$"Square {number} is {squareNumber}"};
+
+int squareNumber = (int)Math.Pow(number, power)
+
