95cd8abd3cd82ba344efdd1d7f4cb5f4c6e9ff31
[Gaia <[dzhayadzoy@gmail.com]>
2024-02-25 18:43:27 +0500

    Lesson_3

---
+++ Java OOP_February/Lesson_3/FlowComparator.java
@@ -0,0 +1,21 @@
+package Lesson_3;
+
+import java.util.Comparator;
+
+// — Создать класс StreamComparator, реализующий сравнение количества групп, входящих в Поток;
+public class  FlowComparator implements Comparator<Flow> {
+
+    Flow flow;
+
+
+
+
+
+
+
+    @Override
+    public int compareTo(StudentGroup o) {
+        
+    }
+    
+}
